ebd4d7a88162309e098889ddcfe5f1ae685da3c5
John Doe <ssramya06@gmail.com>
2022-05-27 00:19:53 +0800

    deploy smart contarct

--- truffle-config.js
+++ truffle-config.js
@@ -12,13 +12,13 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*"
+      network_id: "*",
     },
     test: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*"
-    }
+      network_id: "*",
+    },
   },
 
   //
